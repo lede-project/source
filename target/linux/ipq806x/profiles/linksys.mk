ab81367608824afbc79dedf781a8259151b0fbf7
Adrian Panella <ianchi74@outlook.com>
2016-04-05 21:48:35 -0500

    profile: add usb storage default support
    Signed-off-by: Adrian Panella <ianchi74@outlook.com>

--- target/linux/ipq806x/profiles/linksys.mk
+++ target/linux/ipq806x/profiles/linksys.mk
@@ -12,6 +12,9 @@ define Profile/EA8500
 		kmod-usb-core kmod-usb-ohci kmod-usb2 \
 		kmod-usb3 kmod-usb-dwc3-qcom kmod-usb-phy-qcom-dwc3 \
 		kmod-ath10k wpad-mini ath10k-firmware-qca99x0 \
+		kmod-usb-storage-extras kmod-fs-msdos \
+		kmod-fs-ntfs kmod-fs-ext4 kmod-fs-cifs \
+		kmod-nls-utf8 kmod-nls-iso8859-1 kmod-nls-cp437 \
 		uboot-envtools caldata-utils
 endef
 
