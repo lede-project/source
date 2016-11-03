cafcdbcd3aae118acb631a4be7cf922810433a9f
Florian Larysch <fl@n621.de>
2016-11-03 14:30:53 +0100

    toolchain: remove powerpc64 feature
    The powerpc64 feature flag was introduced with the PS3 support, which
    has been removed for quite a while and is now unused. Remove it and the
    special biarch handling it triggered during the toolchain build.
    
    Signed-off-by: Florian Larysch <fl@n621.de>

--- scripts/target-metadata.pl
+++ scripts/target-metadata.pl
@@ -32,7 +32,6 @@ sub target_config_features(@) {
 		/fpu/ and $ret .= "\tselect HAS_FPU\n";
 		/spe_fpu/ and $ret .= "\tselect HAS_SPE_FPU\n";
 		/ramdisk/ and $ret .= "\tselect USES_INITRAMFS\n";
-		/powerpc64/ and $ret .= "\tselect powerpc64\n";
 		/nommu/ and $ret .= "\tselect NOMMU\n";
 		/mips16/ and $ret .= "\tselect HAS_MIPS16\n";
 		/rfkill/ and $ret .= "\tselect RFKILL_SUPPORT\n";
