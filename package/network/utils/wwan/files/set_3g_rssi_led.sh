c507b5ab68f7a36550ea5edafe1f0400d5ff35be
Giuseppe Lippolis <giu.lippolis@gmail.com>
2017-01-16 19:52:56 +0100

    wwan: adding 3G rssi led indication
    Another example for the Archer MR200 is added and tested (by heinzek).
    
    Signed-off-by: Giuseppe Lippolis <giu.lippolis@gmail.com>
    Signed-off-by: Henryk Heisig <hyniu@o2.pl>

--- package/network/utils/wwan/files/set_3g_rssi_led.sh
+++ package/network/utils/wwan/files/set_3g_rssi_led.sh
@@ -5,65 +5,128 @@
 set_led_rssi_off() {
 	case $board in
 	dwr-512-b)
-		echo '0' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-		echo '0' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+		echo '0' > /sys/class/leds/$board\:green\:sigstrength/brightness
+		echo '0' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
+		;;
+	mr200)
+		echo '0' > /sys/class/leds/$board:white:signal1/brightness
+		echo '0' > /sys/class/leds/$board:white:signal2/brightness
+		echo '0' > /sys/class/leds/$board:white:signal3/brightness
+		echo '0' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
 		;;
 	esac
 }
 set_led_rssi_1() {
         case $board in
         dwr-512-b)
-                echo '0' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '255' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '0' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '255' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:signal1/brightness
+		echo '0' > /sys/class/leds/$board:white:signal2/brightness
+		echo '0' > /sys/class/leds/$board:white:signal3/brightness
+		echo '0' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
 }
 set_led_rssi_2() {
         case $board in
         dwr-512-b)
-                echo '255' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '255' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '255' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '255' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:signal1/brightness
+		echo '255' > /sys/class/leds/$board:white:signal2/brightness
+		echo '0' > /sys/class/leds/$board:white:signal3/brightness
+		echo '0' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
 }
 set_led_rssi_3() {
         case $board in
         dwr-512-b)
-                echo '255' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '128' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '255' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '128' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:signal1/brightness
+		echo '255' > /sys/class/leds/$board:white:signal2/brightness
+		echo '255' > /sys/class/leds/$board:white:signal3/brightness
+		echo '0' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
 }
 set_led_rssi_4() {
         case $board in
         dwr-512-b)
-                echo '255' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '0' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '255' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '0' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:signal1/brightness
+		echo '255' > /sys/class/leds/$board:white:signal2/brightness
+		echo '255' > /sys/class/leds/$board:white:signal3/brightness
+		echo '255' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
 }
 set_led_rssi_5() {
         case $board in
         dwr-512-b)
-                echo '255' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '0' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'none' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '255' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '0' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'none' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:signal1/brightness
+		echo '255' > /sys/class/leds/$board:white:signal2/brightness
+		echo '255' > /sys/class/leds/$board:white:signal3/brightness
+		echo '255' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'none' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
 }
 set_led_rssi_err() {
         case $board in
         dwr-512-b)
-                echo '0' > /sys/class/leds/dwr-512-b\:green\:sigstrength/brightness
-                echo '255' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/brightness
-                echo 'timer' >   /sys/class/leds/dwr-512-b\:red\:sigstrength/trigger
+                echo '0' > /sys/class/leds/$board\:green\:sigstrength/brightness
+                echo '255' >   /sys/class/leds/$board\:red\:sigstrength/brightness
+                echo 'timer' >   /sys/class/leds/$board\:red\:sigstrength/trigger
                 ;;
+	mr200)
+		echo '0' > /sys/class/leds/$board:white:signal1/brightness
+		echo '0' > /sys/class/leds/$board:white:signal2/brightness
+		echo '0' > /sys/class/leds/$board:white:signal3/brightness
+		echo '255' > /sys/class/leds/$board:white:signal4/brightness
+		echo 'timer' > /sys/class/leds/$board:white:signal4/trigger
+		;;
         esac
+}
+set_lte_led_on() {
+	case $board in
+	mr200)
+		echo '255' > /sys/class/leds/$board:white:4g/brightness
+		;;
+	esac
+}
+set_lte_led_off() {
+	case $board in
+		mr200)
+		echo '0' > /sys/class/leds/$board:white:4g/brightness
+	;;
+	esac
 }
 
 board=$(ramips_board_name)
@@ -72,6 +135,8 @@ case "$1" in
 start)
 	until [ 0 -gt 1 ]; do
 		rssi3g=$(gcom -d /dev/ttyUSB0 -s /etc/gcom/getstrength.gcom|awk '/[CSQ]/ { print $2 }'|sed -e 's/,//')
+		sleep 2
+		lte=$(gcom -d /dev/ttyUSB1 reg |awk -F[,] '/^\Registered/ {print $2}')
 
 		if [ $rssi3g -eq 99 ]; then
 			set_led_rssi_err
@@ -87,6 +152,12 @@ start)
 			set_led_rssi_1
 		else
 			set_led_rssi_err;
+		fi
+
+		if [ $lte -eq 7 ]; then
+			set_lte_led_on
+		else
+			set_lte_led_off;
 		fi
 
 		sleep 15;
@@ -107,6 +178,10 @@ test)
 	set_led_rssi_5
 	sleep 2;
 	set_led_rssi_err
+	sleep 2;
+	set_lte_led_on
+	sleep 2;
+	set_lte_led_off
 	sleep 2;
 	set_led_rssi_off
 esac
