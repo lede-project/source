945c5ec19fa84235c623eeb3eea0521a86dd5473
am2dk <347006777@qq.com>
2017-02-02 17:10:19 +0800

    add bord inform support for new devices
    TP-Link 882 886 941nv7 6300v2 bord inform

--- target/linux/ar71xx/files/arch/mips/ath79/machtypes.h
+++ target/linux/ar71xx/files/arch/mips/ath79/machtypes.h
@@ -4,6 +4,7 @@
  *  Copyright (C) 2008-2010 Gabor Juhos <juhosg@openwrt.org>
  *  Copyright (C) 2008 Imre Kaloz <kaloz@openwrt.org>
  *
+ 
  *  This program is free software; you can redistribute it and/or modify it
  *  under the terms of the GNU General Public License version 2 as published
  *  by the Free Software Foundation.
