4dd506edc2e8088c57636822ecf1b6ce915f06f1
am2dk <347006777@qq.com>
2017-02-02 17:12:49 +0800

    add support for tplink-wmac.h
    tplink-wmac.h should be needed

--- target/linux/ar71xx/files/arch/mips/ath79/tplink-wmac.h
+++ target/linux/ar71xx/files/arch/mips/ath79/tplink-wmac.h
@@ -3,3 +3,4 @@ void tplink_register_ap91_wmac1(size_t offset, u8 *mac, int mac_inc);
 
 void tplink_register_builtin_wmac2(size_t offset, u8 *mac, int mac_inc);
 void tplink_register_ap91_wmac2(size_t offset, u8 *mac, int mac_inc);
+
