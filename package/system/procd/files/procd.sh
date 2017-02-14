openwrt/openwrt

commit f88e3a4c0abb60bb76a7678dd30dfdc8a808a2f1

procd: add default timeout for reload trigger actions
Signed-off-by: Felix Fietkau <nbd@nbd.name>

package/system/procd/files/procd.sh
@@ -32,6 +32,7 @@
 
 . $IPKG_INSTROOT/usr/share/libubox/jshn.sh
 
+PROCD_RELOAD_DELAY=1000
 _PROCD_SERVICE=
 
 _procd_call() {
@@ -221,6 +222,11 @@ _procd_set_param() {
 	esac
 }
 
+_procd_add_timeout() {
+	[ "$PROCD_RELOAD_DELAY" -gt 0 ] && json_add_int "" "$PROCD_RELOAD_DELAY"
+	return 0
+}
+
 _procd_add_interface_trigger() {
 	json_add_array
 	_procd_add_array_data "$1"
@@ -240,6 +246,8 @@ _procd_add_interface_trigger() {
 
 	json_close_array
 	json_close_array
+
+	_procd_add_timeout
 }
 
 _procd_add_reload_interface_trigger() {
@@ -271,6 +279,8 @@ _procd_add_config_trigger() {
 	json_close_array
 
 	json_close_array
+
+	_procd_add_timeout
 }
 
 _procd_add_raw_trigger() {

lede-project/source

commit 2ca86bb5a52b1da1f99943f21198741fa6d8dad9

procd: fix default timeout for reload trigger actions
Default trigger action timeout was added to procd.sh in commit f88e3a4c0
(procd: add default timeout for reload trigger actions)
However, the timeout value was not placed under the correct JSON-script
array nesting level and thus did not apply.

To fix this and make the timeout actually apply to the reload triggers,
we place it in the correct scope, that is the per-trigger array.

Fixes: f88e3a4c0abb60bb76a7678dd30dfdc8a808a2f1
Signed-off-by: Denis Osvald <denis.osvald@sartura.hr>

package/system/procd/files/procd.sh
@@ -251,9 +251,8 @@ _procd_add_interface_trigger() {
 	json_close_array
 
 	json_close_array
-	json_close_array
-
 	_procd_add_timeout
+	json_close_array
 }
 
 _procd_add_reload_interface_trigger() {
@@ -283,10 +282,8 @@ _procd_add_config_trigger() {
 	json_close_array
 
 	json_close_array
-
-	json_close_array
-
 	_procd_add_timeout
+	json_close_array
 }
 
 _procd_add_raw_trigger() {