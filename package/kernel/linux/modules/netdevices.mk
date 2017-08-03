2eb64b7868822684631aa0b1ab9ea48c17b300f0
Philip Prindeville <philipp@redfish-solutions.com>
2017-08-03 00:00:45 -0600

    e1000e: change module params to something saner
    InterruptThrottleRate 4 is "simplified" and gives best performance
    when RPS is enabled.  IntMode 1 (MSI) performs better than MSI-X
    when the number of PCI or PCIe devices is quite small (as is the
    case for most embedded systems).
    
    Signed-off-by: Philip Prindeville <philipp@redfish-solutions.com>

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -494,6 +494,9 @@ define KernelPackage/e1000e
   DEPENDS:=@PCIE_SUPPORT +kmod-ptp
   KCONFIG:=CONFIG_E1000E
   FILES:=$(LINUX_DIR)/drivers/net/ethernet/intel/e1000e/e1000e.ko
+  MODPARAMS.e1000e:= \
+	IntMode=1 \
+	InterruptThrottleRate='4,4,4,4,4,4,4,4'
   AUTOLOAD:=$(call AutoProbe,e1000e)
 endef
 
