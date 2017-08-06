de743053e4e68b3f9f6c6d586bbb21a21f7b50ae
Chuanhong Guo <gch981213@gmail.com>
2017-08-06 15:03:34 +0800

    修改部分型号flash大小为8m

--- target/linux/ar71xx/image/tp-link.mk
+++ target/linux/ar71xx/image/tp-link.mk
@@ -927,7 +927,7 @@ define Device/tl-wr841-v7
 endef
 
 define Device/tl-wr841-v8
-  $(Device/tplink-4mlzma)
+  $(Device/tplink-8mlzma)
   DEVICE_TITLE := TP-LINK TL-WR841N/ND v8
   BOARDNAME := TL-WR841N-v8
   DEVICE_PROFILE := TLWR841
@@ -935,7 +935,7 @@ define Device/tl-wr841-v8
 endef
 
 define Device/tl-wr841-v9
-  $(Device/tplink-4mlzma)
+  $(Device/tplink-8mlzma)
   DEVICE_TITLE := TP-LINK TL-WR841N/ND v9
   BOARDNAME := TL-WR841N-v9
   DEVICE_PROFILE := TLWR841
