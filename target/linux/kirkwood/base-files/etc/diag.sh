d9930d9b52f80853c8bf8161bc838518bc63e3a1
Tong Zhang <ztong@vt.edu>
2017-10-16 16:29:51 -0400

    add basic pogoplug v4 support   u-boot support comes later

--- target/linux/kirkwood/base-files/etc/diag.sh
+++ target/linux/kirkwood/base-files/etc/diag.sh
@@ -14,6 +14,9 @@ get_status_led() {
 	pogo_e02)
 		status_led="pogo_e02:orange:fault"
 		;;
+	pogo_v4)
+		status_led="pogoplugv4:red:fault"
+		;;
 	linksys-audi)
 		status_led="audi:green:power"
 		;;
