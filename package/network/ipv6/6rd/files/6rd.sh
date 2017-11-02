c8d7efd0de202552b0de7f93d48be3d53de41d5f
yangfl <yangfl@users.noreply.github.com>
2017-11-02 13:54:10 +0800

    6rd: use pure shell 6rdcalc
    Signed-off-by: David Yang <mmyangfl@gmail.com>

--- package/network/ipv6/6rd/files/6rd.sh
+++ package/network/ipv6/6rd/files/6rd.sh
@@ -9,6 +9,76 @@
 	init_proto "$@"
 }
 
+get_cidr_netbit() {
+	eval $1=$(echo $2 | cut -d/ -f2)
+}
+
+ip6addr_to_hex() {
+	eval $1=$(echo $2 | cut -d/ -f1 | sed "s/::/$(echo $2 |
+		awk -F':' '{for(i=1;i<=10-NF;i++)printf ":"}')/" |
+		awk -F':' '{for(i=1;i<=NF;i++)x=x""sprintf("%04s", $i);gsub(/ /,"0",x);printf x}')
+}
+
+hex_to_ip6addr() {
+	eval $1=$(echo $2 | sed 's/.\{4\}/&:/g;s/:$//;s/:0\+/:/g;s/:::*/::/g')
+}
+
+ip4addr_to_hex() {
+	eval $1=$(echo $2 | cut -d/ -f1 | awk -F'\.' '{for(i=1;i<=NF;i++)printf "%02x",$i}')
+}
+
+hex_to_bin() {
+	eval $1=$(echo $2 | sed '
+	s/0/0000/g;s/1/0001/g;s/2/0010/g;s/3/0011/g;s/4/0100/g;s/5/0101/g;s/6/0110/g;s/7/0111/g;s/8/1000/g;
+	s/9/1001/g;s/a/1010/g;s/b/1011/g;s/c/1100/g;s/d/1101/g;s/e/1110/g;s/f/1111/g')
+}
+
+bin_to_hex() {
+	padding=$(printf "%$(((${#2}+3)/4*4-${#2}))s")
+	eval $1=$(echo ${padding// /0}$2 | sed 's/.\{4\}/& /g;
+	s/0000/0/g;s/0001/1/g;s/0010/2/g;s/0011/3/g;s/0100/4/g;s/0101/5/g;s/0110/6/g;s/0111/7/g;s/1000/8/g;
+	s/1001/9/g;s/1010/a/g;s/1011/b/g;s/1100/c/g;s/1101/d/g;s/1110/e/g;s/1111/f/g;s/ //g')
+}
+
+# $1 | ($2 << $3)
+hex_or_lshift() {
+	local __dst __src __padding __res
+	hex_to_bin __dst $2
+	hex_to_bin __src $3
+	__padding=$(printf "%$4s")
+	__src=$__src${__padding// /0}
+	if [ ${#__src} -gt ${#__dst} ]; then
+		__padding=$(printf "%$((${#__src}-${#__dst}))s")
+		__dst=${__padding// /0}$__dst
+	elif [ ${#__dst} -gt ${#__src} ]; then
+		__padding=$(printf "%$((${#__dst}-${#__src}))s")
+		__src=${__padding// /0}$__src
+	fi
+	__res=
+	for i in `seq 0 $((${#__dst}-1))`; do
+		if [ ${__dst:$i:1} = '0' -a ${__src:$i:1} = '0' ]; then
+			__res=${__res}0
+		else
+			__res=${__res}1
+		fi
+	done
+	bin_to_hex __res $__res
+	eval $1=$__res
+}
+
+sixrd_calc() {
+	local __ip6prefix_hex __ip6prefixlen __ipaddr_hex __ip4hostlen __ip6subnet __ip6hostlen
+	ip6addr_to_hex __ip6prefix_hex $2
+	__ip6prefixlen=$3
+	ip4addr_to_hex __ipaddr_hex $4
+	__ip4hostlen=$((32-$5))
+	__ipaddr_hex=$(printf "%x" $((0x$__ipaddr_hex&((1<<$__ip4hostlen)-1))))
+	__ip6hostlen=$((128-$__ip6prefixlen-$__ip4hostlen))
+	hex_or_lshift __ip6subnet $__ip6prefix_hex $__ipaddr_hex $__ip6hostlen
+	hex_to_ip6addr __ip6subnet $__ip6subnet
+	eval $1=$__ip6subnet/$((128-__ip6hostlen))
+}
+
 proto_6rd_setup() {
 	local cfg="$1"
 	local iface="$2"
@@ -44,7 +114,8 @@ proto_6rd_setup() {
 	ip4prefix="${ip4prefix#NETWORK=}"
 
 	# Determine our IPv6 address.
-	local ip6subnet=$(6rdcalc "$ip6prefix/$ip6prefixlen" "$ipaddr/$ip4prefixlen")
+	local ip6subnet
+	sixrd_calc ip6subnet $ip6prefix $ip6prefixlen $ipaddr $ip4prefixlen
 	local ip6addr="${ip6subnet%%::*}::1"
 
 	# Determine the IPv6 prefix
