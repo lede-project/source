1094e09056ce843a90361d8a823a42fb4ca50a2a
greenbreakfast <lazardemin@gmail.com>
2019-01-29 19:09:44 +0000

    prep for building additional packages

--- scripts/onion-feed-setup.sh
+++ scripts/onion-feed-setup.sh
@@ -6,6 +6,6 @@ echo "Preparing package feeds..."
 ./scripts/feeds install -a -p onion
 
 # take care of Onion customized packages
-#./scripts/feeds uninstall bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
-#./scripts/feeds install -p onion bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
+./scripts/feeds uninstall bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
+./scripts/feeds install -p onion bluez-examples bluez-libs bluez-utils pulseaudio-daemon pulseaudio-profiles pulseaudio-tools avrdude
 
