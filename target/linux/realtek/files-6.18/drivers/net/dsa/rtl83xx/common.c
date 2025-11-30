19478cf334c33849246bdce1d67c0d0e574d34ad
Jonas Jelonek <jelonek.jonas@gmail.com>
2025-11-30 12:34:33 +0000

    realtek: dsa: 6.18: adjust mac ops and callbacks
    Between 6.12 and 6.18, several DSA switch ops have changed.
    phylink_mac_* ops were moved out of dsa_switch_ops into dsa_switch using
    phylink_mac_ops. Add new definitions for that and adjust the callbacks
    accordingly to work with the different signatures.
    
    get_mac_eee() has been dropped and support_eee() was added.
    
    remove_new() has been dropped completely, allowing us to use remove()
    again.
    
    Signed-off-by: Jonas Jelonek <jelonek.jonas@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/21181
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/realtek/files-6.18/drivers/net/dsa/rtl83xx/common.c
+++ target/linux/realtek/files-6.18/drivers/net/dsa/rtl83xx/common.c
@@ -1632,6 +1632,7 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 	switch (soc_info.family) {
 	case RTL8380_FAMILY_ID:
 		priv->ds->ops = &rtldsa_83xx_switch_ops;
+		priv->ds->phylink_mac_ops = &rtldsa_83xx_phylink_mac_ops;
 		priv->cpu_port = RTL838X_CPU_PORT;
 		priv->port_mask = 0x1f;
 		priv->port_width = 1;
@@ -1642,6 +1643,7 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL8390_FAMILY_ID:
 		priv->ds->ops = &rtldsa_83xx_switch_ops;
+		priv->ds->phylink_mac_ops = &rtldsa_83xx_phylink_mac_ops;
 		priv->cpu_port = RTL839X_CPU_PORT;
 		priv->port_mask = 0x3f;
 		priv->port_width = 2;
@@ -1652,6 +1654,7 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL9300_FAMILY_ID:
 		priv->ds->ops = &rtldsa_93xx_switch_ops;
+		priv->ds->phylink_mac_ops = &rtldsa_93xx_phylink_mac_ops;
 		priv->cpu_port = RTL930X_CPU_PORT;
 		priv->port_mask = 0x1f;
 		priv->port_width = 1;
@@ -1663,6 +1666,7 @@ static int rtl83xx_sw_probe(struct platform_device *pdev)
 		break;
 	case RTL9310_FAMILY_ID:
 		priv->ds->ops = &rtldsa_93xx_switch_ops;
+		priv->ds->phylink_mac_ops = &rtldsa_93xx_phylink_mac_ops;
 		priv->cpu_port = RTL931X_CPU_PORT;
 		priv->port_mask = 0x3f;
 		priv->port_width = 2;
