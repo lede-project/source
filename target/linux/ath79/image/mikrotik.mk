immortalwrt/immortalwrt

commit 13fd9a9ce037a877f9f6f4117b5a524811b2fbee

ath79: remove kmod-ath9k and wpad from Mikrotik RB750r2
The RB750r2 (HEXLite) does not have wifi and those packages bloat the image
by a significant amount. When building a custom image with WireGuard and
booting that from initramfs, there wasn't enough space left in tmpfs to
upload and flash the squashfs image. Investigating what packages I could
remove, I discovered these unneeded ones.

Signed-off-by: Eicke Herbertz <wolletd@posteo.de>
Link: https://github.com/openwrt/openwrt/pull/22134
Signed-off-by: Robert Marko <robimarko@gmail.com>
(cherry picked from commit 42cd48678745d5a99720e03595c53230a3c078f9)
Link: https://github.com/openwrt/openwrt/pull/22475
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/ath79/image/mikrotik.mk
@@ -24,6 +24,7 @@ define Device/mikrotik_routerboard-750-r2
   $(Device/mikrotik_nor)
   SOC := qca9533
   DEVICE_MODEL := RouterBOARD 750 r2 (hEX lite)
+  DEVICE_PACKAGES := -kmod-ath9k -wpad-basic-mbedtls
   IMAGE_SIZE := 16256k
   SUPPORTED_DEVICES += rb-750-r2
 endef

lede-project/source

commit a25e9cf3622f5ede13bc761a6e51d2a3ae6fd443

ath79: mikrotik: fix DEVICE_PACKAGES
Instead of redifining the DEVICE_PACKAGES, make sure to expand the list
so yafut is excluded by default as well by $(Device/mikrotik_nor)

Fixes: 13fd9a9ce037a ("ath79: remove kmod-ath9k and wpad from Mikrotik RB750r2")
Signed-off-by: Robert Marko <robimarko@gmail.com>
(cherry picked from commit dfbaf846501b1f17d4ac916bb87ad84773414ed8)
Signed-off-by: Ryan Leung <untilscour@protonmail.com>
Link: https://github.com/openwrt/openwrt/pull/22667
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/ath79/image/mikrotik.mk
@@ -24,7 +24,7 @@ define Device/mikrotik_routerboard-750-r2
   $(Device/mikrotik_nor)
   SOC := qca9533
   DEVICE_MODEL := RouterBOARD 750 r2 (hEX lite)
-  DEVICE_PACKAGES := -kmod-ath9k -wpad-basic-mbedtls
+  DEVICE_PACKAGES += -kmod-ath9k -wpad-basic-mbedtls
   IMAGE_SIZE := 16256k
   SUPPORTED_DEVICES += rb-750-r2
 endef