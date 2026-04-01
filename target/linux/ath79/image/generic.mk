52d776ac968f59149afcd65f8aa1a1d130343ace
Rosen Penev <rosenp@gmail.com>
2026-04-01 16:34:53 -0700

    ath79: wndap360: add owl loader
    Needed so that the WiFi interfaces come up properly.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>
    Link: https://github.com/openwrt/openwrt/pull/22735
    Signed-off-by: Robert Marko <robimarko@gmail.com>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -2234,6 +2234,7 @@ define Device/netgear_wndap360
   $(Device/netgear_generic)
   SOC := ar7161
   DEVICE_MODEL := WNDAP360
+  DEVICE_PACKAGES := kmod-owl-loader
   IMAGE_SIZE := 7744k
   BLOCKSIZE := 256k
   KERNEL := kernel-bin | append-dtb | gzip | uImage gzip
